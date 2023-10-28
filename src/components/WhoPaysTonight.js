34ea5811a957a300e1a3464390d11004206824cc
compgirl123 <claudia.f.feochari@hotmail.com>
2023-10-28 17:42:48 -0400

    Fixed bug with randomizating function

--- src/components/WhoPaysTonight.js
+++ src/components/WhoPaysTonight.js
@@ -9,13 +9,11 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 const WhoPaysTonight = () => {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState('');
-  const [arrayIndex, setArrayIndex] = useState(0);
-  const [chosenPerson, setChosenPerson] = useState('');
+  const [chosenPerson, setChosenPerson] = useState(null);
 
   useEffect(() => {
     console.log("Updated todos:", todos);
   }, [todos]);
-
 
   const addToDo = (todo) => {
     if (todo) {
@@ -36,46 +34,51 @@ const WhoPaysTonight = () => {
   }
 
   const handleWhoPays = () => {
-    setArrayIndex(Math.floor(Math.random()* todos.length));
-    setChosenPerson(todos[arrayIndex].task);
+    if (todos.length > 0) {
+      const randomIndex = Math.floor(Math.random() * todos.length);
+      const randomPerson = todos[randomIndex];
+      setChosenPerson(randomPerson);
+    } else {
+      setChosenPerson(null);
+    }
   }
 
   return (
     <>
-      <div className="toDoWrapper" style={{ display: 'flex', flexDirection: 'row' }}>
-      <div className="partOne" style={{ width: '50%', border: '1px solid blue'  }}>
-        <form className="todoform" onSubmit={handleSubmit}>
-        <div className="input-container">
-          <InputField
-            type="text"
-            placeholder="Add a person"
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          </div>
-          <div className="button-container">
-            <Button type="submit" className="todo-btn">
-              Add
-            </Button>
-          </div>
-        </form>
-        {todos.length > 0 && todos.map((todo, index) => (
-          <div className="toDo" key={todo.id}>
-            <p>{todo.task}</p>
-            <FontAwesomeIcon className="deleteIcon" icon={faTrash} onClick={() => deleteToDo(todo.id)} />
-          </div>
-        ))}
+      <div className="toDoWrapper" >
+        <div className="partOne" style={{ width: '50%', border: '1px solid blue' }}>
+          <form className="todoform" onSubmit={handleSubmit}>
+            <div className="input-container">
+              <InputField
+                type="text"
+                placeholder="Add a person"
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+              />
+            </div>
+            <div className="button-container">
+              <Button type="submit" className="todo-btn">
+                Add
+              </Button>
+            </div>
+          </form>
+          {todos.length > 0 && todos.map((todo, index) => (
+            <div className="toDo" key={todo.id}>
+              <p>{todo.task}</p>
+              <FontAwesomeIcon className="deleteIcon" icon={faTrash} onClick={() => deleteToDo(todo.id)} />
+            </div>
+          ))}
+        </div>
+        <div className="partTwo hidden-part" style={{ width: '50%', border: '1px solid blue', justifyContent: 'center', height: '100%' }}>
+          <Button type="submit" className="todo-btn" onClick={handleWhoPays}>
+            Who Will Pay Tonight?
+          </Button>
+          <br/>
+          <h2 style={{ textAlign: 'center'}}>{chosenPerson ? 'Chosen Person: ' + chosenPerson.task : ''}</h2>
+        </div>
       </div>
-      <div className="partTwo" style={{ width: '50%', border: '1px solid blue', display: 'flex', justifyContent: 'center', height: '100%' }}>
-      <Button type="submit" className="todo-btn" onClick={() => handleWhoPays() }>
-          Who Will Pay Tonight?
-        </Button>
-        <h2>Chosen Person:{chosenPerson}</h2>
-      </div>
-    </div>
     </>
   );
 }
 
 export default WhoPaysTonight;
-
